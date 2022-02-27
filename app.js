a76bdd0e336c0f0987a6873116edf60a4bff843c
fabiochidda <fabio.chidda99@gmail.com>
2022-02-27 23:20:11 +0100

    Aggiunti gli oggetti in JS ed aggiunto il toggle per segnare la task completata (anche se non funziona come dovrebbe)

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const app = new Vue ({
     el: "#app",
     data: {
         titolo: "To Do List",
-        
+        done: false,
+        toDoList: [
+            {
+                text: "Finire questo esercizio",
+                done: true,
+            },
+            {
+                text: "Formattare il pc",
+                done: true,
+            },
+            {
+                text: "Andare avanti su Elden Ring",
+                done: true,
+            }
+        ]
 
-
-
-
-
+    },
+    methods: {
+        toggle () {
+            this.done = !this.done
+        }
     }
 
 
